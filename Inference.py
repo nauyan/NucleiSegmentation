f2d8918f195dadc5a56bbcc39c944d0ef56f3bdd
root <nauyan@hotmail.com>
2020-06-30 19:17:16 +0500

    Plots

--- Inference.py
+++ Inference.py
@@ -6,6 +6,8 @@ import os
 from skimage import io
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 import numpy as np
+import keras
+import segmentation_models as sm
 
 save_dir = "results/sample_images/"
 
@@ -16,9 +18,15 @@ prepared_dataset_dir = "prepared_dataset"
 images_list = glob.glob(os.path.join(dataset_dir,prepared_dataset_dir,"train","images")+"/*.png")[0]
 image = img_to_array(load_img(images_list, color_mode='rgb'))
 mask = img_to_array(load_img(os.path.join(dataset_dir,prepared_dataset_dir,"train","masks")+"/"+os.path.basename(images_list), color_mode='grayscale'))
-print(image.shape,mask.shape)
+model = sm.Unet('efficientnetb0', classes=1, activation='sigmoid')
+model = keras.models.load_model("results/weights/cpm15_Best.h5", compile=False)
+image = image.reshape(1,256,256,3)
+pred = model.predict(image/255.0)
+pred = pred.reshape(256,256,1)
+image = image.reshape(256,256,3)
 io.imsave(os.path.join(save_dir, dataset_name + ".png"), image)
 io.imsave(os.path.join(save_dir, dataset_name + "_mask.png"), mask*255.0)
+io.imsave(os.path.join(save_dir, dataset_name + "_pred.png"), pred*255.0)
 
 dataset_name = "cpm17"
 dataset_dir = "dataset/cpm17"
@@ -26,9 +34,15 @@ prepared_dataset_dir = "prepared_dataset"
 images_list = glob.glob(os.path.join(dataset_dir,prepared_dataset_dir,"train","images")+"/*.png")[0]
 image = img_to_array(load_img(images_list, color_mode='rgb'))
 mask = img_to_array(load_img(os.path.join(dataset_dir,prepared_dataset_dir,"train","masks")+"/"+os.path.basename(images_list), color_mode='grayscale'))
-print(image.shape,mask.shape)
+model = sm.Unet('efficientnetb0', classes=1, activation='sigmoid')
+model = keras.models.load_model("results/weights/cpm17_Best.h5", compile=False)
+image = image.reshape(1,256,256,3)
+pred = model.predict(image/255.0)
+pred = pred.reshape(256,256,1)
+image = image.reshape(256,256,3)
 io.imsave(os.path.join(save_dir, dataset_name + ".png"), image)
 io.imsave(os.path.join(save_dir, dataset_name + "_mask.png"), mask*255.0)
+io.imsave(os.path.join(save_dir, dataset_name + "_pred.png"), pred*255.0)
 
 dataset_name = "consep"
 dataset_dir = "dataset/consep"
@@ -36,9 +50,15 @@ prepared_dataset_dir = "prepared_dataset"
 images_list = glob.glob(os.path.join(dataset_dir,prepared_dataset_dir,"train","images")+"/*.png")[0]
 image = img_to_array(load_img(images_list, color_mode='rgb'))
 mask = img_to_array(load_img(os.path.join(dataset_dir,prepared_dataset_dir,"train","masks")+"/"+os.path.basename(images_list), color_mode='grayscale'))
-print(image.shape,mask.shape)
+model = sm.Unet('efficientnetb0', classes=1, activation='sigmoid')
+model = keras.models.load_model("results/weights/consep_Best.h5", compile=False)
+image = image.reshape(1,256,256,3)
+pred = model.predict(image/255.0)
+pred = pred.reshape(256,256,1)
+image = image.reshape(256,256,3)
 io.imsave(os.path.join(save_dir, dataset_name + ".png"), image)
 io.imsave(os.path.join(save_dir, dataset_name + "_mask.png"), mask*255.0)
+io.imsave(os.path.join(save_dir, dataset_name + "_pred.png"), pred*255.0)
 
 dataset_name = "nucleiseg"
 dataset_dir = "dataset/nucleiseg"
@@ -46,11 +66,15 @@ prepared_dataset_dir = "prepared_dataset"
 images_list = glob.glob(os.path.join(dataset_dir,prepared_dataset_dir,"train","images")+"/*.png")[200]
 image = img_to_array(load_img(images_list, color_mode='rgb'))
 mask = img_to_array(load_img(os.path.join(dataset_dir,prepared_dataset_dir,"train","masks")+"/"+os.path.basename(images_list), color_mode='grayscale'))
-print(image.shape,mask.shape)
+model = sm.Unet('efficientnetb0', classes=1, activation='sigmoid')
+model = keras.models.load_model("results/weights/nucleiseg_Best.h5", compile=False)
+image = image.reshape(1,256,256,3)
+pred = model.predict(image/255.0)
+pred = pred.reshape(256,256,1)
+image = image.reshape(256,256,3)
 io.imsave(os.path.join(save_dir, dataset_name + ".png"), image)
 io.imsave(os.path.join(save_dir, dataset_name + "_mask.png"), mask*255.0)
+io.imsave(os.path.join(save_dir, dataset_name + "_pred.png"), pred*255.0)
 
-
-print(np.unique(mask))
 
 
